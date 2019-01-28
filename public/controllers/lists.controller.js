c8c4234b3f7a18e326dc086f6a36c2b6b55894f9
Dario Gabriel Mincioni <dario.mincioni@gmail.com>
2019-01-28 17:12:54 +0000

    more lists app progress

--- public/controllers/lists.controller.js
+++ public/controllers/lists.controller.js
@@ -1,10 +1,21 @@
 app.controller('listsController', ['$scope', 'listsService', 'iconService', 'inventoryService', function ($scope, listsService, iconService, inventoryService) {
 
-    $scope.currentCraftList = listsService.lists.craft_lists.general;
+    $scope.lists = listsService.lists;
+
+    $scope.currentCraftList = listsService.lists.craft_lists.lessgeneral;
 
     $scope.convertIcon = iconService.convertIcon;
 
     $scope.inventory = inventoryService.inventory;
 
+    $scope.hasIngredients = {currentCraftList: false};
+
+    listsService.generateRequiredIngredients('lessgeneral');
+
+    $scope.requiredIngredients = listsService.requiredIngredients;
+
+    $scope.setUnsavedInv = function () {
+        inventoryService.unsavedChanges = true;
+    };
 
 }]);
